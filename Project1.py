3103041713142bda6b946b437de1a606d8588f34
MaryamTheCoder1 <maryamm.kaleemm@gmail.com>
2026-02-11 21:28:58 +0500

    updated my project by adding dificulty modes

--- Project1.py
+++ Project1.py
@@ -26,15 +26,39 @@
 
 import random
 
-num = random.randint(1, 100)
+print("Select Difficulty: ")
+print("1. Easy (1 - 10)")
+print("2. Medium (1 - 50)")
+print("3. Hard (1 - 100)")
+
+choice = input("Enter choice (1/2/3): ")
+
+if choice == "1":
+    max_range = 10
+elif choice == "2":
+    max_range = 50
+else:
+    max_range = 100
+
+num = random.randint(1, max_range)
 guesses = 0
 
 while True:
-    guess = input("Guess the number between 1 and 100: ")
+    guess = input("Guess the number between 1 and {max_range} (or type 'exit'): ")
     if guess == "exit":
         print("Game Exit!")
         break
+
+    if not guess.isdigit():
+        print("Enter numbers only")
+        continue
+
     guess = int(guess)
+
+    if guess < 1 or guess > max_range:
+        print("Number is out of range")
+        continue
+
     guesses += 1
     if guess < num:
         print("Your guess is less than the number")
